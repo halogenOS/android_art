d483b27c027a550a5c96613334a9216a75890252
Simao Gomes Viana <xdevs23@outlook.com>
2017-03-09 09:43:03 +0100

    ART optimization
    This change improves overall ART performance by preloading some more
    classes and using more optimization on the native code.
    
    In future, there might come more changes to this. This was the first
    experiment on that topic, and as it is working, we can add more later.
    
    Change-Id: I655400cbb5afa8d6f6c52412941b47a807ed9310

--- compiler/Android.mk
+++ compiler/Android.mk
@@ -309,6 +309,8 @@ $$(ENUM_OPERATOR_OUT_GEN): $$(GENERATED_SRC_DIR)/%_operator_out.cc : $(LOCAL_PAT
   art_codegen_targets :=
 endef
 
+LOCAL_CFLAGS += -O3
+
 # We always build dex2oat and dependencies, even if the host build is otherwise disabled, since they are used to cross compile for the target.
 ifeq ($(ART_BUILD_HOST_NDEBUG),true)
   $(eval $(call build-libart-compiler,host,ndebug))
